649156df34ae2c4a1f93bf8bd4b928f2aca7a623
zding5 <zding5@illinois.edu>
2017-02-25 22:25:50 -0600

    cors enabled

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,12 @@ let client = new es.Client({
     log: 'trace'
 });
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/', (req, res)=>{
     res.send("helloworld");
 })
